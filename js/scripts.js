732acc6077701cb6bc1ed11de8b0d4d9791f5cbf
Kukk0006 <112907293+Kukk0006@users.noreply.github.com>
2024-03-29 19:47:04 -0400

    Rough enough for monday

--- js/scripts.js
+++ js/scripts.js
@@ -53,4 +53,15 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
