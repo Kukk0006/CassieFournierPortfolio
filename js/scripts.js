d7ecfdc372c776110719fe6d175ec92c9d3742aa
Kukk0006 <112907293+Kukk0006@users.noreply.github.com>
2024-04-01 00:26:09 -0400

    finishes hobby

--- js/scripts.js
+++ js/scripts.js
@@ -56,6 +56,7 @@ window.addEventListener('DOMContentLoaded', event => {
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
     spaceBetween: 30,
+    loop: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
