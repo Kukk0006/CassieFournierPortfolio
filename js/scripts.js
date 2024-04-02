2f25557ec266d55b99f77fba3b39abaacf1b594d
Kukk0006 <112907293+Kukk0006@users.noreply.github.com>
2024-04-02 12:48:47 -0400

    fixes styling

--- js/scripts.js
+++ js/scripts.js
@@ -85,3 +85,7 @@ var swiper = new Swiper(".mySwiper", {
         },
     },
 });
+let loader = document.getElementById("preloader");
+window.addEventListener('load', function () {
+    loader.style.display = "none"
+})
